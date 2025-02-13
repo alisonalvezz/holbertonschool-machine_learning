39bb2070c5698b3f4bb4c0e94a623982d14c34f8
Alison Alvez <alisonalvez@Alisons-MacBook-Pro.local>
2025-02-13 17:13:25 -0300

    i think it works

--- supervised_learning/classification/2-neuron.py
+++ supervised_learning/classification/2-neuron.py
@@ -11,26 +11,26 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be positive")
-        
+
         self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
-    
+
     @property
     def W(self):
         return self.__W
-    
+
     @property
     def b(self):
         return self.__b
-    
+
     @property
     def A(self):
         return self.__A
-    
+
     def forward_prop(self, X):
         """Calculates the forward propagation of the neuron"""
-        Z = np.matmul(self.__W, X) + self.__b 
+        Z = np.matmul(self.__W, X) + self.__b
         """multiplication between weight and the input matrix plus the bias"""
         self.__A = 1 / (1 + np.exp(-Z))
         """sigmoid/activation function"""
