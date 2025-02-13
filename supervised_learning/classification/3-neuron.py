1d5dc94f81f0b22b15cdaa70165afb45a3f856e5
Alison Alvez <alisonalvez@Alisons-MacBook-Pro.local>
2025-02-13 17:14:21 -0300

    i think it works

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -44,5 +44,5 @@ class Neuron:
     def cost(self, Y, A):
         """Calculates the cost using logistic regression loss function"""
         m = Y.shape[1]
-        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)) / m
         return cost
