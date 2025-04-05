d553556bf6c956b4a4bfdf0dfa08861c7e03aa18
Alison <alisonalvez05@gmail.com>
2025-04-05 12:05:09 -0300

    the western exchange added

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+the western exchange
+"""
+
+
+def np_transpose(matrix):
+    """
+    transposes matrix
+    attributes:
+        matrix: the matrix to transpose
+    return:
+        a new numpy.ndarray
+    """
+    return matrix.transpose()
