f3f1331f6ff949cb48469adcc12188d64cc63484
Alison <alisonalvez05@gmail.com>
2025-04-07 08:58:45 -0300

    line added

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+"""
+line
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
-
+    """
+    Plots y = x^3 from 0 to 10 using a red line.
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
