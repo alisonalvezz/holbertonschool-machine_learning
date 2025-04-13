ed573ba00eb80f36c0c5e95d9fef6fc2252a480d
Alison <alisonalvez05@gmail.com>
2025-04-13 19:37:48 -0300

    added

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+sum total
+"""
+
+def summation_i_squared(n):
+    """
+    calculates the sum of i^2 from 1 to n
+    """
+    if type(n) != int or n < 1:
+        return None
+    return n * (n + 1) * (2 * n + 1) // 6
