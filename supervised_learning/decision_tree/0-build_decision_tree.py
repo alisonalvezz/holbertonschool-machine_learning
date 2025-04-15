ab9881488ac2ca70ace92e67964b935cd49bed70
Alison <alisonalvez05@gmail.com>
2025-04-15 14:32:16 -0300

    build decision tree

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -5,8 +5,10 @@ build decision tree
 
 import numpy as np
 
+
 class Node:
-    def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
+    def __init__(self, feature=None, threshold=None,\
+            left_child=None, right_child=None, is_root=False, depth=0):
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -18,10 +20,13 @@ class Node:
 
     def max_depth_below(self):
         """
-        Calculates the maximum depth of all nodes below this node, including leaf nodes.
+        Calculates the maximum depth of all nodes below this node, 
+        including leaf nodes.
 
-        This method works recursively, searching for child nodes and calculating their depth,
-        then it returns the maximum depth value among the child nodes, incremented by 1 for the current node.
+        This method works recursively, searching for child nodes and 
+        calculating their depth,
+        then it returns the maximum depth value among the child nodes, 
+        incremented by 1 for the current node.
 
         Returns:
         int: The maximum depth of all nodes below this node.
@@ -38,7 +43,7 @@ class Node:
             return 0
         depth = max(right_depth, left_depth)
         return depth
-        
+
 
 class Leaf(Node):
     def __init__(self, value, depth=None):
@@ -51,7 +56,8 @@ class Leaf(Node):
         return self.depth
 
 class Decision_Tree():
-    def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
+    def __init__(self, max_depth=10, min_pop=1, seed=0,\
+                 split_criterion="random", root=None):
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
